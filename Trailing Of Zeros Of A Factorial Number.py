52533f25c0045bb8a32261e9be72bedee47ab5da
Vaishnavi Uday Nehe <55103460+Vaishnavi-Uday-Nehe@users.noreply.github.com>
2022-06-07 01:05:36 +0530

    Add files via upload

---
+++ Trailing Of Zeros Of A Factorial Number.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#trailingZerosOfFactorial
+n = 100
+count = 0
+
+while(n>=5):
+    n//=5
+    count+=n
+print("nos of zeros at the end of factorial of 100 is:",count)
+
+#codebyvaishnaviudaynehe
+
+
+# In[ ]:
+
+
+
+
